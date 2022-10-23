0f3af313ec0bba41a42e42529cb37d1bd23e48fb
markyang <1923859@gmail.com>
2022-10-23 10:29:04 +0800

    mod cql_data

--- scripts/process_cql_data.py
+++ scripts/process_cql_data.py
@@ -8,6 +8,14 @@ def process_customer_data(path, output_path):
     # save result
     df.to_csv(output_path, index=False)
 
+def process_orderline_data(path, output_path):
+    df = pd.read_csv(path, header=None)
+    # df[6] = df[6].apply(lambda x: x * 100) # OL_AMOUNT
+
+    # # save result
+    # df.to_csv(output_path, index=False)
+
 
 if __name__ == '__main__':
-    process_customer_data('./data/data_files/customer.csv', './data/data_files/customer_cql.csv')
+    # process_customer_data('./data/data_files/customer.csv', './data/data_files/customer_cql.csv')
+    process_orderline_data('./data/data_files/order-line.csv', './data/data_files/order-line_cql.csv')
