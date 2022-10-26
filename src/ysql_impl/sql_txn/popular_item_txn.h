a90ab92942f44be8f5e924d4677698afa5016f79
yuding.wang <yuding.wang@bytedance.com>
2022-10-26 14:17:33 +0800

    add log for all sql txn

--- src/ysql_impl/sql_txn/popular_item_txn.h
+++ src/ysql_impl/sql_txn/popular_item_txn.h
@@ -7,8 +7,9 @@
 namespace ydb_util {
 class YSQLPopularItemTxn : public PopularItemTxn {
  public:
-  explicit YSQLPopularItemTxn(pqxx::connection* conn)
-      : PopularItemTxn(), conn_(conn) {}
+  explicit YSQLPopularItemTxn(pqxx::connection* conn, std::ofstream& txn_out,
+                              std::ofstream& err_out)
+      : PopularItemTxn(), conn_(conn), txn_out_(txn_out), err_out_(err_out) {}
 
   Status Execute(double* diff_t) noexcept override;
 
@@ -16,6 +17,8 @@ class YSQLPopularItemTxn : public PopularItemTxn {
   std::vector<std::string> outputs;
   static constexpr int MAX_RETRY_COUNT = 3;
   pqxx::connection* conn_;
+  std::ofstream& txn_out_;
+  std::ofstream& err_out_;
 };
 }  // namespace ydb_util
 #endif
