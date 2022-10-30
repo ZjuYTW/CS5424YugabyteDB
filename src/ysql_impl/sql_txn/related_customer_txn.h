fc28d37f2756b55865633cd19dcec059fd61ae7e
markyang <1923859@gmail.com>
2022-10-30 22:38:39 +0800

    fix related customer bug

--- src/ysql_impl/sql_txn/related_customer_txn.h
+++ src/ysql_impl/sql_txn/related_customer_txn.h
@@ -9,9 +9,13 @@
 namespace ydb_util {
 class YSQLRelatedCustomerTxn : public RelatedCustomerTxn {
  public:
-  explicit YSQLRelatedCustomerTxn(pqxx::connection* conn, std::ofstream& txn_out,
+  explicit YSQLRelatedCustomerTxn(pqxx::connection* conn,
+                                  std::ofstream& txn_out,
                                   std::ofstream& err_out)
-      : RelatedCustomerTxn(), conn_(conn), txn_out_(txn_out), err_out_(err_out) {}
+      : RelatedCustomerTxn(),
+        conn_(conn),
+        txn_out_(txn_out),
+        err_out_(err_out) {}
 
   Status Execute(double* diff_t) noexcept;
 
@@ -20,9 +24,10 @@ class YSQLRelatedCustomerTxn : public RelatedCustomerTxn {
                        std::unordered_set<std::string>& customers,
                        pqxx::work* txn);
   pqxx::result getOrdersSQL_(int c_w_id, int c_d_id, int c_id, pqxx::work* txn);
-  pqxx::result getOrderLineSQL_(int o_w_id, int o_d_id, int o_id,
-                                pqxx::work* txn);
-  int getCustomerIdSQL_(int w_id, int d_id, int o_id, pqxx::work* txn);
+  pqxx::result getOrderLineSQL_(std::string o_id, std::string o_d_id,
+                                std::string o_w_id, pqxx::work* txn);
+  int getCustomerIdSQL_(std::string w_id, std::string d_id, std::string o_id,
+                        pqxx::work* txn);
 
   static constexpr int MAX_RETRY_COUNT = 6;
   static constexpr int INCOMMON_THRESHOLD = 2;
