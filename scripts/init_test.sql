ab9f2c6342bb479ee99e616cd61052bed446be87
winston-yan <54773440+winston-yan@users.noreply.github.com>
2022-11-03 15:27:47 +0800

    update YCQL init data script (#45)
    Co-authored-by: yanxw_bytedance <winston.yan@bytedance.com>

--- scripts/init_test.sql
+++ scripts/init_test.sql
@@ -101,7 +101,7 @@ OL_AMOUNT BIGINT,
 OL_SUPPLY_W_ID INT,
 OL_QUANTITY INT,
 OL_DIST_INFO VARCHAR,
-PRIMARY KEY (OL_W_ID,OL_D_ID,OL_O_ID,OL_NUMBER)
+PRIMARY KEY (OL_W_ID,OL_I_ID,OL_D_ID,OL_O_ID,OL_NUMBER)
 );
 
 DROP TABLE IF EXISTS ybtest.stock;
@@ -129,33 +129,33 @@ PRIMARY KEY (S_W_ID,S_I_ID)
 
 
 COPY ybtest.warehouse (W_ID, W_NAME, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP, W_TAX, W_YTD)
-FROM 'data/test_files/warehouse.csv'
+FROM 'data/test_files/warehouse_cql.csv'
 WITH DELIMITER=',' AND NULL='null';
 
 COPY ybtest.district (D_W_ID, D_ID, D_NAME, D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP, D_TAX, D_YTD, D_NEXT_O_ID)
-FROM 'data/test_files/district.csv'
+FROM 'data/test_files/district_cql.csv'
 WITH DELIMITER=',' AND NULL='null';
 
 COPY ybtest.customer (C_W_ID, C_D_ID, C_ID, C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT, C_DELIVERY_CNT, C_DATA)
-FROM 'data/test_files/customer.csv'
-WITH DELIMITER=',' AND NULL='null';
+FROM 'data/test_files/customer_cql.csv'
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=50000;
 
 COPY ybtest.orders (O_W_ID, O_D_ID, O_ID, O_C_ID, O_CARRIER_ID, O_OL_CNT, O_ALL_LOCAL, O_ENTRY_D)
 FROM 'data/test_files/order.csv'
-WITH DELIMITER=',' AND NULL='null';
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=50000;
 
 COPY ybtest.item (I_ID, I_NAME, I_PRICE, I_IM_ID, I_DATA)
-FROM 'data/test_files/item.csv'
-WITH DELIMITER=',' AND NULL='null';
+FROM 'data/test_files/item_cql.csv'
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=50000;
 
 COPY ybtest.orderline (OL_W_ID, OL_D_ID, OL_O_ID, OL_NUMBER, OL_I_ID, OL_DELIVERY_D, OL_AMOUNT, OL_SUPPLY_W_ID, OL_QUANTITY, OL_DIST_INFO)
-FROM 'data/test_files/order-line.csv'
-WITH DELIMITER=',' AND NULL='null';
+FROM 'data/test_files/order-line_cql.csv'
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=100000;
 
 COPY ybtest.stock (S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT, S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10, S_DATA)
-FROM 'data/test_files/stock.csv'
-WITH DELIMITER=',' AND NULL='null';
+FROM 'data/test_files/stock_cql.csv'
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=50000;
 
 COPY ybtest.order_max_quantity (O_W_ID, O_D_ID, O_ID, MAX_QUANTITY, ITEM_IDS)
-FROM 'data/test_files/order_max_quantity.csv'
-WITH DELIMITER=',' AND NULL='null';
+FROM 'data/test_files/order_max_quantity_cql.csv'
+WITH DELIMITER=',' AND NULL='null' AND INGESTRATE=50000;
